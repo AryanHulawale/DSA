554948448949fdff498e1ecb0394102d961a7fb5
Aryan Hulawale <aryanhulawale2020@gmail.com>
2026-09-13 22:38:45 +0530

    sorry

---
+++ BASICS/dec_to_bin.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int dec = 47;
+
+    int rem;
+    int binary;
+
+    while(dec != 0){
+        printf("Tomorrow");
+    } 
+
+    return 0;
+}
